d34dcc213c7a837fbbe6faca945bd22d4e85b1d6
AhmedUsama <eng.ahmedusama2012@gmail.com>
2022-06-01 15:19:57 +0200

    My Website

---
+++ test.js
@@ -0,0 +1,37 @@
+//  Start iq
+
+$(document).ready(function(){
+   $("test").ready(function(){
+      $(".YUsama").fadeIn(2000);
+   });
+  });
+
+
+
+$(document).ready(function(){
+   $("test").ready(function(){
+      $(".textname").fadeIn(1000);
+      $(".textjob").fadeIn(1000);
+   });
+  });
+
+
+let demoOffset =$(".More").offset().top;
+
+$(window).scroll(function () { 
+   let wScroll = $(window).scrollTop();
+   if (wScroll > demoOffset){
+      $('.More').eq(0).fadeIn(2000,()=>{
+         $('.about').eq(0).fadeIn(2000, ()=> {
+            $('.Victory').eq(0).fadeIn(2000, ()=>{
+               $('.Calm').eq(0).fadeIn(2000, ()=> {
+                  $('.Skills').eq(0).fadeIn(2000, ()=>{
+                     $('.contact').eq(0).fadeIn(2000);
+                  });
+               });
+            });
+         });
+      });
+   };
+});
+
