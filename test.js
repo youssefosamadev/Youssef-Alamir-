d9c6636709d91699c96bfb9d38e5b8952a4588fd
muhammedsheriff <muhammed.sheriff9999@gmail.com>
2022-06-05 17:50:07 +0200

    adjustment in more and - About - Contact

--- test.js
+++ test.js
@@ -1,40 +1,46 @@
 //  Start iq
-
-$(document).ready(function(){
-   $("test").ready(function(){
-      $(".YUsama").fadeIn(2000);
-   });
+$(document).ready(function () {
+  $("test").ready(function () {
+    $(".YUsama").fadeIn(2000);
   });
-
-
-
-$(document).ready(function(){
-   $("test").ready(function(){
-      $(".textname").fadeIn(1000);
-      $(".textjob").fadeIn(1000);
-   });
-  });
-
-
-let demoOffset =$(".More").offset().top;
-
-$(window).scroll(function () { 
-   let wScroll = $(window).scrollTop();
-   if (wScroll > demoOffset){
-      $('.More').eq(0).fadeIn(2000,()=>{
-         $('.about').eq(0).fadeIn(2000, ()=> {
-            $('.Victory').eq(0).fadeIn(2000, ()=>{
-               $('.Calm').eq(0).fadeIn(2000, ()=> {
-                  $('.Skills').eq(0).fadeIn(2000, ()=>{
-                     $('.contact').eq(0).fadeIn(2000);
-                  });
-               });
-            });
-         });
-      });
-   };
 });
 
+$(document).ready(function () {
+  $("test").ready(function () {
+    $(".textname").fadeIn(1000);
+    $(".textjob").fadeIn(1000);
+  });
+});
 
+let demoOffset = $(".More").offset().top;
 
-
+$(window).scroll(function () {
+  let wScroll = $(window).scrollTop();
+  if (wScroll > demoOffset) {
+    $(".More")
+      .eq(0)
+      .fadeIn(2000, () => {
+        $(".about")
+          .eq(0)
+          .fadeIn(2000, () => {
+            $(".Victory")
+              .eq(0)
+              .fadeIn(2000, () => {
+                $(".Calm")
+                  .eq(0)
+                  .fadeIn(2000, () => {
+                    $(".Skills")
+                      .eq(0)
+                      .fadeIn(2000, () => {
+                        $(".contact").eq(0).fadeIn(2000);
+                      });
+                  });
+              });
+          });
+      });
+  }
+});
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
